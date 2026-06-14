19937bd08b98f204347cea4459b14e6598700994
Muez Fathi <muez_4801@limu.edu.ly>
2026-06-14 21:27:40 +0200

    fix: remove hardcoded credentials from providers.tf

--- providers.tf
+++ providers.tf
@@ -9,8 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "61e822a8-758e-427e-9026-e3b5d46fdd22"
-  client_id       = "de021fdd-06ac-413f-b247-0868fad8089c"
-  tenant_id       = "a8a6d952-dc1e-4b83-97e1-18ff70df777f"
-}
-
+} 
