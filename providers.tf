53829e576d674d70ea9fe6cf18fa382b75dd9cbf
muez-4801 <muez_4801@limu.edu.ly>
2026-06-14 21:49:17 +0200

    Update
    Removed the terraform block and added authentication details for the azurerm provider.

--- providers.tf
+++ providers.tf
@@ -1,12 +1,8 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-} 
+  
+  subscription_id = "61e822a8-758e-427e-9026-e3b5d46fdd22"
+  tenant_id       = "a8a6d952-dc1e-4b83-97e1-18ff70df777f"
+  client_id       = "de021fdd-06ac-413f-b247-0868fad8089c"
+  client_secret   = "1GC8Q~AzmZAMKZxCKX2cZ7IH50SEldEEidFlXawz"
+}
